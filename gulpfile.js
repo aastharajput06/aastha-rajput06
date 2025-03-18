47cc28e0bc39ca0b931f20555894d0a08f22b6e7
Aastha Rajput <aastharajput967@gmail.com>
2025-03-18 02:36:25 -0400

    git commit -m "Update skills list"

--- gulpfile.js
+++ gulpfile.js
@@ -6,49 +6,45 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
-// Sass taskk
-gulp.task('sass', function () {
+// Sass task
+gulp.task('sass', function(done) {
+  console.log('Running sass task...');
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
+    .on('end', done);
 });
 
 // Jekyll task
 gulp.task('jekyll', function(done) {
-  require('child_process').spawn('jekyll', ['build'], { stdio: 'inherit' });
-  done();
+  console.log('Running jekyll task...');
+  require('child_process').spawn('jekyll', ['build'], { stdio: 'inherit' })
+    .on('close', done);
 });
 
-// Copy Jekyll output to dist, exclude package-lock.json
-gulp.task('copy-jekyll', function() {
-  return gulp.src(['_site/**/*', '!_site/package-lock.json'])
-    .pipe(gulp.dest('dist'));
-});
-
-// JS task (optional)
-gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
-
-// Images task (optional)
-gulp.task('images', function() {
-  return gulp.src('src/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+// Copy Jekyll output to dist
+gulp.task('copy-jekyll', function(done) {
+  console.log('Running copy-jekyll task...');
+  return gulp.src(['_site/**/*', '!_site/package-lock.json'], { allowEmpty: true })
+    .pipe(gulp.dest('dist'))
+    .on('end', () => {
+      console.log('copy-jekyll completed');
+      done();
+    });
 });
 
 // Build task
-gulp.task('build', gulp.series('jekyll', 'sass', 'copy-jekyll' /*, 'js', 'images' */));
+gulp.task('build', gulp.series('jekyll', 'sass', 'copy-jekyll'));
 
 // Serve task
 gulp.task('serve', function(done) {
+  console.log('Running serve task...');
   browserSync.init({
-    server: './dist'
+    server: './dist',
+    port: 3002
   });
   gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
   done();
