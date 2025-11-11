1260a944e1c2c34b384d8012e8d29bee66758af3
Aastha Rajput <aastharajput090@gmail.com>
2025-11-11 14:07:19 -0500

    Files updated, installed dependencies

--- _site/dist/js/main.js
+++ _site/dist/js/main.js
@@ -1,20 +1,20 @@
 $(function() {
   const d = new Date();
   const hours = d.getHours();
-  const night = hours >= 19 || hours <= 7; // between 7pm and 7am
+  const night = hours >= 19 || hours <= 7; // between 7pm & 7am
   const body = document.querySelector('body');
   const toggle = document.getElementById('toggle');
   const input = document.getElementById('switch');
 
   if (night) {
     input.checked = true;
-    body.classList.add('light');
+    body.classList.add('night');
   }
 
   toggle.addEventListener('click', function() {
     const isChecked = input.checked;
     if (isChecked) {
-      body.classList.remove('light');
+      body.classList.remove('night');
     } else {
       body.classList.add('night');
     }
